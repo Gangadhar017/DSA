e34a3de046d5477b0ba13b4c218fb8e8bb92bf02
Gangadhar017 <156058881+Gangadhar017@users.noreply.github.com>
2024-11-20 11:56:20 +0530

    Add files via upload

---
+++ lecture_01/Test.java
@@ -0,0 +1,13 @@
+package lecture_01;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Coding Blocks");
+		System.out.print("Hello");
+		System.out.println("Bye");
+
+	}
+
+}
